4ba21bdf9ddffda4b3f3b02537e339a680342029
charlieabhishek <abcptech@gmail.com>
2019-03-26 01:38:05 +0900

    Nativa and Web communication.

--- HybridAppDemo/HybridAppDemo/ViewController.swift
+++ HybridAppDemo/HybridAppDemo/ViewController.swift
@@ -8,24 +8,60 @@
 
 import UIKit
 import WebKit
+import JavaScriptCore
 
-class ViewController: UIViewController, WKUIDelegate {
+class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
     
     var webView: WKWebView!
     
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        view = webView
-    }
+//    override func loadView() {
+//        super.loadView()
+//
+//        let contentController = WKUserContentController()
+//        contentController.add(self, name: "callback")
+//
+//        let config = WKWebViewConfiguration()
+//        config.userContentController = contentController
+//
+//        webView = WKWebView(frame: view.frame, configuration: config)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let contentController = WKUserContentController()
+        contentController.add(self, name: "callback")
+        
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        
+        let userScript = WKUserScript(
+            source: "callJSFunc()",
+            injectionTime: WKUserScriptInjectionTime.atDocumentEnd,
+            forMainFrameOnly: true
+        )
+        
+        contentController.addUserScript(userScript)
+        
+        webView = WKWebView(frame: view.frame, configuration: config)
         let path = Bundle.main.path(forResource: "web/demo", ofType: "html")
         let myURL = URL(fileURLWithPath: path!)
         let myRequest = URLRequest(url: myURL)
         webView.load(myRequest)
+        self.view.addSubview(webView);
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        //This function handles the events coming from javascript. We'll configure the javascript side of this later.
+        //We can access properties through the message body, like this:
+        guard let response = message.body as? String else{
+            return
+        }
+        print("response = \(response)")
+        sendToJavaScript(name: "Abhishek", age: 28)
+    }
+    
+    func sendToJavaScript(name:String,age:Int){
+        webView.evaluateJavaScript("callJSFunc()", completionHandler: nil)
     }
 }
 
--- HybridAppDemo/HybridAppDemo/web/demo.js
+++ HybridAppDemo/HybridAppDemo/web/demo.js
@@ -1,8 +1,21 @@
 var app = angular.module('demoApp', []);
-app.controller('demoCtrl', function($scope) {
-   console.log('demo Controller'); 
 
-   $scope.callNativeBtnAction = function(){
-      console.log('callNativeBtnAction'); 
-   }
+app.controller('demoCtrl', function ($scope) {
+   console.log('demo Controller');
+
+   $scope.nativeMessage = '';
+
+   $scope.callNativeBtnAction = function () {
+      console.log('callNativeBtnAction');
+      try {
+         webkit.messageHandlers.callback.postMessage("Web to Native Message");
+       } catch(err) {
+         console.log('Can not reach native code');
+       }
+   };
+
+function callJSFunc(){
+      console.log("callJSFunc called");
+  };
+               
 });
